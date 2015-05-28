656583be8b3fb43318cd055e82685ef52fc78402
Paul Piscuc <paul.piscuc@gmail.com>
2015-05-28 23:17:59 +0200

    added all the components

---
+++ src/components/search.react.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Search = React.createClass({
+    render: function () {
+        //var church_name = this.props.settings.church_name;
+        //var church_address = this.props.settings.church_address;
+        //var church_city = this.props.settings.church_city;
+        return (
+            <div>welcome to react</div>
+        );
+    }
+});
+
+module.exports = Search;
--- src/routes.js
+++ src/routes.js
@@ -12,24 +12,28 @@ var RouteHandler = Router.RouteHandler;
 // var NewMember = require('./components/newmember.jsx');
 // var Settings = require('./components/settings.jsx');
 // var Dashboard = require('./components/dashboard.jsx');
-//
-//
-// var routes = (
-//           <Route name="app" path="/" handler={MainApp}>
-//              <Route name="search" handler={Search}>
-//                 <Route name="info" path=":memberId" handler={MemberDetail} />
-//                 <DefaultRoute handler={SearchStats} />
-//             </Route>
-//
-//              <Route name="newdata" handler={NewMember} />
-//              <Route name="settings" handler={Settings} />
-//              <DefaultRoute handler={Dashboard} />
-//            </Route>
-// );
+var Container = require('./components/container.react');
+var Search = require('./components/search.react');
+var MemberDetail = require('./components/memberdetail.react');
+var SearchStats = require('./components/searchstats.react');
+var NewMember = require('./components/newmember.react');
+var Settings = require('./components/settings.react');
 var Dashboard = require('./components/dashboard.react');
 
+
 var routes = (
-    <Route name="dashboard" path="/" handler={Dashboard} />
+    <Route name="app" path="/" handler={Container}>
+        <Route name="search" handler={Search}>
+            <Route name="info" path=":memberId" handler={MemberDetail} />
+            <DefaultRoute handler={SearchStats} />
+        </Route>
+        <Route name="newdata" handler={NewMember} />
+        <Route name="settings" handler={Settings} />
+        <DefaultRoute name="dashboard" handler={Dashboard} />
+    </Route>
 );
+
+
+
 
 module.exports = routes;
